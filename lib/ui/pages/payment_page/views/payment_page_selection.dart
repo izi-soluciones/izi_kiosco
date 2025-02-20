64378cce5517421d026145dc6b5443d054160195
Alvin Jamil Poma Tarqui <48572620+pomaalvin@users.noreply.github.com>
2025-02-20 14:04:25 -0400

    #86dw1xtfd-Se ajusta el modal par aque se cierre por el tiempo de inactividad

--- lib/ui/pages/payment_page/views/payment_page_selection.dart
+++ lib/ui/pages/payment_page/views/payment_page_selection.dart
@@ -67,6 +67,7 @@ class PaymentPageSelection extends StatelessWidget {
 
     CustomAlerts.defaultAlert(
         context: context,
+        dismissible: true,
         child: WarningModal(
             onAccept: ()async{
               GoRouter.of(context).goNamed(RoutesKeys.home);
