f7892a61809629dc48623303d5b814ca1e2e96a4
Alvin Jamil Poma Tarqui <48572620+pomaalvin@users.noreply.github.com>
2026-09-23 15:50:00 -0400

    fix(colombia): redondeo half-up y 2 decimales en moneda extranjera (BKB-008)

--- lib/domain/utils/calc_utils.dart
+++ lib/domain/utils/calc_utils.dart
@@ -13,33 +13,10 @@ class Calc {
   static double div(num a, num b) =>
       (Decimal.parse(a.toString()) / Decimal.parse(b.toString())).toDouble();
 
-  static double roundConservador(num n, [int decimals = 2]) {
-    Decimal numero = Decimal.parse(n.toString());
-    final numString = numero.toString();
-    final dotIndex = numString.indexOf('.');
-
-    if (dotIndex == -1) return numero.toDouble();
-
-    final decimalDecisorPos = dotIndex + decimals + 1;
-    final decimalDecisor = (decimalDecisorPos < numString.length) ? numString[decimalDecisorPos] : '0';
-
-    if (decimalDecisor == '5') {
-      final decimalSiguienteChar = (decimalDecisorPos + 1 < numString.length) ? numString[decimalDecisorPos + 1] : '0';
-      final int decimalSiguiente = int.tryParse(decimalSiguienteChar) ?? 0;
-
-      if (decimalSiguiente % 2 != 0) {
-        if (numero >= Decimal.zero) {
-          return numero.ceil(scale: decimals).toDouble();
-        } else {
-          return numero.floor(scale: decimals).toDouble();
-        }
-      } else {
-        return numero.truncate(scale: decimals).toDouble();
-      }
-    } else {
-      return numero.round(scale: decimals).toDouble();
-    }
-  }
+  // Medio hacia arriba (el 5 sube, lejos de cero), igual que el backbone, izi-app, izi-pos e
+  // izi-ms-ventas (BKB-008): si uno redondea distinto, el total no cuadra con la factura.
+  static double roundConservador(num n, [int decimals = 2]) =>
+      Decimal.parse(n.toString()).round(scale: decimals).toDouble();
 
   static double roundCeil(num a) =>
       Decimal.parse(a.toString()).ceil(scale: 2).toDouble();
